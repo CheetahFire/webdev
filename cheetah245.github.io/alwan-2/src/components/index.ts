f399d18c674fd886b403c8090beddf3ae3c08244
cheetah245 <laszlo1@live.nl>
2023-11-30 15:52:55 +0100

    colorpickers
    - added extra color pickers
    - changed their default state to white
    - changed them to alter global variables instead of the element variables

--- cheetah245.github.io/alwan-2/src/components/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './Palette';
-export * from './Utility';
-export * from './Sliders';
-export * from './Inputs';
-export * from './Swatches';
-export * from './Reference';
--- js/FP_main.js
+++ js/FP_main.js
@@ -15,10 +15,13 @@ let nextRotate_tres = 0;
 
 function rotateGradient_uno() {
     currentRotate_uno = nextRotate_uno;
+    uno.style['-moz-transform'] = 'rotate(' + currentRotate_uno + 'deg)';
     uno.style.transform = 'rotate(' + currentRotate_uno + 'deg)';
-    uno_back.style.transform = 'rotate(' + currentRotate_uno/3.6 + 'deg)';
-
+    
+    uno_back.style.transform = 'rotate(' + currentRotate_uno / 3.6 + 'deg)';
+  
     nextRotate_uno = currentRotate_uno + 1;
+
 }
 function rotateGradient_dos() {
     currentRotate_dos = nextRotate_dos;
@@ -32,19 +35,65 @@ function rotateGradient_tres() {
 }
 
 
-var intervalID = window.setInterval(rotateGradient_uno, 5);
-var intervalID = window.setInterval(rotateGradient_dos, 5);
-var intervalID = window.setInterval(rotateGradient_tres, 5);
+var intervalID = setInterval(rotateGradient_uno, 5);
+var intervalID = setInterval(rotateGradient_dos, 10);
+var intervalID = setInterval(rotateGradient_tres, 15);
 
 const alwan = new Alwan('#color-picker', {
-
+    color: 'white'
+});
+const alwan2 = new Alwan('#color-picker2', {
+    color: 'white'
+});
+const alwan3 = new Alwan('#color-picker3', {
+    color: 'white'
+});
+const alwan4 = new Alwan('#color-picker4', {
+    color: 'white'
+});
+const alwan5 = new Alwan('#color-picker5', {
+    color: 'white'
+});
+const alwan6 = new Alwan('#color-picker6', {
+    color: 'white'
+});
+const alwan7 = new Alwan('#color-picker7', {
+    color: 'white'
+});
+const alwan8 = new Alwan('#color-picker8', {
+    color: 'white'
 });
 
 alwan.on('change', (ev) => {
     ev.hex;
-    console.log(ev.hex);
-    for (const red of document.getElementsByClassName("red")) {
-        red.style.setProperty('--gradient-color-1', `${ev.hex}`);
-    };
-
+    document.documentElement.style.setProperty('--gradient-color-1', `${ev.hex}`);
 });
+alwan2.on('change', (ev) => {
+    ev.hex;
+    document.documentElement.style.setProperty('--gradient-color-2', `${ev.hex}`);
+});
+alwan3.on('change', (ev) => {
+    ev.hex;
+    document.documentElement.style.setProperty('--gradient-color-3', `${ev.hex}`);
+});
+alwan4.on('change', (ev) => {
+    ev.hex;
+    document.documentElement.style.setProperty('--gradient-color-4', `${ev.hex}`);
+});
+alwan5.on('change', (ev) => {
+    ev.hex;
+    document.documentElement.style.setProperty('--gradient-color-5', `${ev.hex}`);
+});
+alwan6.on('change', (ev) => {
+    ev.hex;
+    document.documentElement.style.setProperty('--gradient-color-6', `${ev.hex}`);
+});
+alwan7.on('change', (ev) => {
+    ev.hex;
+    document.documentElement.style.setProperty('--gradient-color-7', `${ev.hex}`);
+});
+alwan8.on('change', (ev) => {
+    ev.hex;
+    document.documentElement.style.setProperty('--gradient-color-8', `${ev.hex}`);
+});
+
